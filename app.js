cde68399e666915cebcc752a8c8e5d60574906c8
sholayman99 <mdsholayman998@gmail.com>
2023-09-12 15:53:42 +0600

    final submission

--- app.js
+++ app.js
@@ -10,8 +10,31 @@ const app = express();
 require("dotenv").config();
 const router = require("./src/Routes/api");
 
-//implementations of middlewares
+//importing security middlewares
+const cors = require('cors');
+const hpp = require('hpp');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const { rateLimit } = require('express-rate-limit');
 
+
+
+const limiter = rateLimit({
+	windowMs: 15 * 60 * 1000, // 15 minutes
+	max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+	standardHeaders: 'draft-7', // draft-6: RateLimit-* headers; draft-7: combined RateLimit header
+	legacyHeaders: false, // X-RateLimit-* headers
+	// store: ... , // Use an external store for more precise rate limiting
+})
+
+
+//implementations of security middlewares.
+
+app.use(cors());
+app.use(hpp());
+app.use(helmet());
+app.use(mongoSanitize());
+app.use(limiter);
 
 
 
